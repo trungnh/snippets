db9f8f65d0ff37bcd40d628c3ceb7667960ce783
Trung Nguyen Huu <trungnh@nissho-vn.com>
2016-10-07 15:59:56 +0700

    Classify crypt function

--- en_decrypt.php
+++ en_decrypt.php
@@ -1,11 +1,40 @@
 <?php 
+class Crypt {
+
+/**
+* Gets a random value for encryption
+* - From php.net docs
+*
+* @param $iv_len
+*  length of random variable
+*/
+function getRndIV($iv_len) 
+{
+	$iv = '';
+	while ($iv_len-- > 0) {
+		$iv .= chr(mt_rand() & 0xff);
+	}
+	return $iv;
+}
+
+/**
+* Encrypts string
+* - From php.net docs
+*
+* @param $str
+*   string to encrypt
+* @param $salt
+*   password to use for encryption
+* @param $iv_len
+*   length of random number
+*/
 function encrypt($str, $salt, $iv_len = 16) 
 {
 	$str .= "\x13";
 	$n = strlen($str);
 	if ($n % 16) $str .= str_repeat("\0", 16 - ($n % 16));
 	$i = 0;
-	$enc_text = getRndIV($iv_len);
+	$enc_text = $this->getRndIV($iv_len);
 	$iv = substr($salt ^ $enc_text, 0, 512);
 	while ($i < $n) {
 		$block = substr($str, $i, 16) ^ pack('H*', md5($iv));
@@ -16,14 +45,17 @@ function encrypt($str, $salt, $iv_len = 16)
 	return base64_encode($enc_text);
 }
 
-function getRndIV($iv_len) {
-	$iv = '';
-	while ($iv_len-- > 0) {
-		$iv .= chr(mt_rand() & 0xff);
-	}
-	return $iv;
-}
-  
+/**
+* Decrypts string
+* - From php.net docs
+*
+* @param $enc
+*   encrypted string to decrypt
+* @param $salt
+*   password to use for encryption
+* @param $iv_len
+*   length of random number
+*/
 function decrypt($enc, $salt, $iv_len = 16) 
 {
 	$enc = base64_decode($enc);
@@ -39,6 +71,4 @@ function decrypt($enc, $salt, $iv_len = 16)
 	}
 	return preg_replace('/\\x13\\x00*$/', '', $str);
 }
-
-
-echo decrypt('TApLhw5VrmFrwE7WgoUzcq8UHGNrHGOrXrYSzDiqzMc=', 'abc');
+}
